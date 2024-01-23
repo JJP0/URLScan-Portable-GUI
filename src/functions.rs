4fefe457667e3475a02f940ac79d27cc0b1492dd
JJP0 <jp.withal@gmail.com>
2024-01-23 23:28:15 +0000

    Add core files

---
+++ src/functions.rs
@@ -0,0 +1,403 @@
+use std::fs::{OpenOptions, File};
+use std::io::{self, Write, BufRead, BufReader, Read};
+use std::collections::HashMap;
+use reqwest;
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue, CONTENT_TYPE};
+use mini_redis::{client, Result};
+use serde_json::{json, Value};
+use serde::{Serialize, Deserialize};
+use reqwest::StatusCode;
+use std::any::type_name;
+
+/*
+Need to refactor:
+1. Create func to get headers as they are standard throughout, repeated code
+2. Create single func for getting api key/uuid
+3. Create single func for saving api key/uuid to file
+4. Refactor whole chunks of code, repeated and not optimal
+5. Test edge cases
+6. Implement more error handling (particularly for invalid URLs and for when 'Fetch results' is clicked before any data has loaded)
+
+
+*/
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Root {
+    pub data: Data,
+    pub lists: Lists,
+    //pub meta: Meta,
+    //pub page: Page,
+    //pub scanner: Scanner,
+    //pub stats: Stats,
+    //pub submitter: Submitter,
+    //pub task: Task,
+    pub verdicts: Verdicts,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Data {
+    pub requests: Vec<Value>,
+    pub cookies: Vec<Value>,
+    pub console: Vec<Value>,
+    pub links: Vec<Value>,
+    pub timing: Timing,
+    pub globals: Vec<Value>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Timing {
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Lists {
+    pub ips: Vec<String>,
+    pub countries: Vec<String>,
+    pub asns: Vec<Value>,
+    pub domains: Vec<Value>,
+    pub servers: Vec<Value>,
+    pub urls: Vec<String>,
+    pub link_domains: Vec<Value>,
+    pub certificates: Vec<Value>,
+    pub hashes: Vec<Value>,
+}
+
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Stats {
+    #[serde(rename = "IPv6Percentage")]
+    pub ipv6percentage: i64,
+    pub ad_blocked: i64,
+    pub domain_stats: Vec<Value>,
+    pub ip_stats: Vec<Value>,
+    pub malicious: i64,
+    pub protocol_stats: Vec<Value>,
+    pub reg_domain_stats: Vec<Value>,
+    pub resource_stats: Vec<Value>,
+    pub secure_percentage: i64,
+    pub secure_requests: i64,
+    pub server_stats: Vec<Value>,
+    pub tls_stats: Vec<Value>,
+    pub total_links: i64,
+    pub uniq_countries: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Verdicts {
+    pub overall: Overall,
+    pub urlscan: Urlscan,
+    pub engines: Engines,
+    pub community: Community,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Overall {
+    pub score: i64,
+    pub categories: Vec<Value>,
+    pub brands: Vec<Value>,
+    pub tags: Vec<Value>,
+    pub malicious: bool,
+    pub has_verdicts: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Urlscan {
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Engines {
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Community {
+    pub score: i64,
+    pub categories: Vec<Value>,
+    pub brands: Vec<Value>,
+    pub votes_total: i64,
+    pub votes_malicious: i64,
+    pub votes_benign: i64,
+    pub malicious: bool,
+    pub has_verdicts: bool,
+}
+
+
+// Custom struct for JSON produced from API search
+// #[derive(Debug, Deserialize)]
+// struct SearchResponse {
+// 	api: String,
+// 	//country: String,
+// 	//message: String,
+// 	//options: String,
+// 	//result: String,
+// 	//url: String,
+// 	uuid: String,
+// 	//visibility: String,
+// }
+
+
+
+
+#[derive(Debug, Deserialize)]
+struct SuccessResponse {
+    uuid: String,
+    api: String,
+    visibility: String,
+    url: String,
+    result: String,
+    options: serde_json::Value,
+    country: Option<String>,
+    message: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ErrorResponse {
+    message: String,
+    description: String,
+    status: u16,
+    errors: Vec<ErrorDetail>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ErrorDetail {
+    title: String,
+    detail: String,
+    status: u16,
+}
+
+#[derive(Debug, Deserialize)]
+enum ApiResponse {
+    Success(SuccessResponse),
+    Error(ErrorResponse),
+}
+
+pub fn save_api_key(api_key: &String) -> io::Result<()> {
+
+	let file_path = "key.txt";
+
+	let mut file = OpenOptions::new()
+		.write(true)
+		.create(true)
+		.truncate(true)
+		.open(file_path)?;
+
+	file.write_all(api_key.as_bytes())?;
+
+	Ok(())
+}
+
+pub fn get_api_key() -> io::Result<String> {
+
+	let file_path = match File::open("key.txt") {
+		Ok(file) => file,
+		Err(e) => {
+			File::create("key.txt").expect("Failed to create key file");
+;			eprintln!("Error opening file: {}", e);
+			return Ok(String::new());
+		}
+	};
+
+	let reader = BufReader::new(file_path);
+
+	if let Some(Ok(line)) = reader.lines().next() {
+		Ok(line)
+	} else {
+		Ok(String::new())
+	}
+}
+
+
+
+
+fn write_uuid_to_file(uuid: &String) -> io::Result<()> {
+	
+	let file_path = "uuid.txt";
+
+		let mut file = OpenOptions::new()
+			.write(true)
+			.create(true)
+			.truncate(true)
+			.open(file_path)?;
+
+		file.write_all(uuid.as_bytes())?;
+
+		Ok(())
+}
+
+
+pub fn get_uuid() -> io::Result<String> {
+	let file_path = match File::open("uuid.txt") {
+		Ok(file) => file,
+		Err(e) => {
+			File::create("uuid.txt").expect("Failed to create uuid file");
+			eprintln!("Error opening file: {}", e);
+			return Ok(String::new());
+		}
+	};
+
+	//let
+
+	let reader = BufReader::new(file_path);
+
+	if let Some(Ok(line)) = reader.lines().next() {
+		Ok(line)
+	} else {
+		Ok(String::new())
+	}
+}
+
+
+#[tokio::main]
+pub async fn scan_url(url: String, api_key: String) -> Result<()> {
+
+	let mut data = HashMap::new();
+	let mut headers = HeaderMap::new();
+
+	let CUSTOM_HEADER: &'static str = "api-key";
+
+	headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+	headers.insert(HeaderName::from_static(CUSTOM_HEADER), HeaderValue::from_str(&api_key).expect("FAILURE HERE"));
+
+	let temp = api_key.to_string();
+
+	data.insert("url", url.to_string());
+	data.insert("visibility", "public".to_string());
+
+	let json_data = &serde_json::json!({
+		"url": url,
+		"visibility": "public"
+	});
+	
+	let client = reqwest::Client::new();
+	let res = client.post("https://urlscan.io/api/v1/scan/")
+		.headers(headers)
+		.json(json_data)
+		.send()
+		.await?;
+
+
+	let status = res.status();
+    let body = res.text().await?;
+
+    println!("{}", status);
+
+    if status.is_success() {
+        let json_result: SuccessResponse = serde_json::from_str(&body).expect("Failed to deserialize response");
+        println!("{:#?}", json_result);
+        let uuid = json_result.uuid;
+        match write_uuid_to_file(&uuid) {
+            Ok(()) => println!("UUID ({}) Successfully added to file", uuid),
+            Err(_) => println!("Error occurred writing UUID"),
+        }
+    } else {
+        println!("Received non-success status code: {}", status);
+        let error_response: ErrorResponse = serde_json::from_str(&body).expect("Failed to deserialize error response");
+        println!("{:#?}", error_response);
+    }
+ 
+	Ok(())
+}
+
+#[tokio::main]
+pub async fn fetch_results() -> Result<()> {
+
+	let api_key = get_api_key().expect("Failed to get API Key");
+	let uuid = get_uuid().expect("Failed to get UUID");
+
+	let mut headers = HeaderMap::new();
+
+	let CUSTOM_HEADER: &'static str = "api-key";
+
+	headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+	headers.insert(HeaderName::from_static(CUSTOM_HEADER), HeaderValue::from_str(&api_key).expect("FAILURE HERE"));
+
+
+	let url = format!("https://urlscan.io/api/v1/result/{}/", uuid);
+
+	let client = reqwest::Client::new();
+
+	let res = client.get(url)
+		.headers(headers)
+		.send()
+		.await?
+		.text()
+		.await?;
+
+	let response: Root = serde_json::from_str(&res)?;
+	
+	let ips = &response.lists.ips;
+	let countries = &response.lists.countries;
+	let urls = &response.lists.urls;
+	let overall_score = response.verdicts.overall.score;
+	let overall_malicious = response.verdicts.overall.malicious;
+	let community_score = response.verdicts.community.score;
+	let community_malicious = response.verdicts.community.malicious;
+
+	let json_data = json!({
+        "ips": ips,
+        "urls": urls,
+        "countries": countries,
+        "overall_score": overall_score,
+        "overall_malicious": overall_malicious,
+        "community_score": community_score,
+        "community_malicious": community_malicious,
+    });
+
+	write_result_data(&json_data)?;
+
+	Ok(())
+}
+
+fn write_result_data(data: &Value) -> io::Result<()> {
+	let file_path = "results.json";
+
+    let mut file = File::create(file_path)?;
+
+    // Serialize the JSON data and write it to the file
+    let serialized_data = serde_json::to_string_pretty(data)?;
+    file.write_all(serialized_data.as_bytes())?;
+
+    println!("Data written to {}", file_path);
+
+	Ok(())
+}
+
+
+#[derive(Debug, Deserialize)]
+pub struct ResultsData {
+    pub community_malicious: bool,
+    pub community_score: i64,
+    pub countries: Vec<String>,
+    pub ips: Vec<String>,
+    pub overall_malicious: bool,
+    pub overall_score: i64,
+    pub urls: Vec<String>,
+}
+
+
+pub fn load_data() -> ResultsData {
+    let json_data = read_json_file("results.json").expect("Failed to read JSON file");
+    serde_json::from_str(&json_data).expect("Failed to deserialize JSON data")
+}
+
+
+fn read_json_file(file_path: &str) -> io::Result<String> {
+    let mut file = File::open(file_path)?;
+    let mut json_data = String::new();
+    file.read_to_string(&mut json_data)?;
+    Ok(json_data)
+}
+/*
+
+fetch reuslts when not yet finished - "{\n  \"message\": \"Scan is not finished yet\",\n  \"status\": 404,\n  \"errors\": [\n    {\n      \"title\": \"Scan is not finished yet\",\n      \"detail\": \"Scan is not finished yet\",\n      \"status\": 404\n    }\n  ]\n}"
+
+
+*/
---
+++ src/main.rs
@@ -0,0 +1,258 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+use eframe::egui::{menu, Color32, widgets::Separator, widgets::Spinner};
+use reqwest;
+use serde_json::{json, Value};
+use serde::{Serialize, Deserialize};
+
+mod functions; 
+use functions::ResultsData;
+
+/*
+Need to refactor:
+1. Create func to get headers as they are standard throughout, repeated code
+2. Create single func for getting api key/uuid
+3. Create single func for saving api key/uuid to file
+4. Refactor whole chunks of code, repeated and not optimal
+5. Test edge cases
+6. Implement more error handling (particularly for invalid URLs and for when 'Fetch results' is clicked before any data has loaded)
+
+
+*/
+
+
+#[derive(Debug, Deserialize)]
+struct SearchResponse {
+    api: String,
+    country: String,
+    message: String,
+    //options: String,
+    result: String,
+    url: String,
+    uuid: String,
+    visibility: String,
+}
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        min_window_size: Some(egui::vec2(520.0, 600.0)),
+        initial_window_size: Some(egui::vec2(520.0, 600.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Portable URLScan GUI",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    )
+}
+
+
+struct MyApp {
+
+    remaining_public_scans: i8,
+    remaining_private_scans: i8,
+    settings_open: bool,
+    api_key: String,
+    url_to_scan: String,
+    url_search_uuid: String,
+    results: ResultsData,
+    current_url_name: String,
+
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+
+        Self {
+
+            remaining_public_scans: 0,
+            remaining_private_scans: 0,
+
+            api_key: functions::get_api_key().expect(""),
+
+            settings_open: false,
+            url_to_scan: "".to_string(),
+            url_search_uuid: "".to_string(),
+            results: ResultsData {
+                community_malicious: Default::default(),
+                community_score: Default::default(),
+                countries: Default::default(),
+                ips: Default::default(),
+                overall_malicious: Default::default(),
+                overall_score: Default::default(),
+                urls: Default::default(),
+            },
+            current_url_name: "".to_string(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+
+            
+            menu::bar(ui, |ui| {
+                ui.menu_button("Settings", |ui| {
+                    if ui.button("API Key").clicked() {
+                        self.settings_open = true;
+                    }
+                });
+            });
+
+            if self.settings_open {
+                egui::Window::new("API Key").show(&ctx, |ui: &mut egui::Ui| {
+                    // Add content to the window here
+                    ui.horizontal(|ui| {
+                        ui.label("API Key");
+                        ui.text_edit_singleline(&mut self.api_key);
+                    });
+
+                    ui.horizontal(|ui| {
+                        if ui.button("Save").clicked() {
+                           // functions::create_json_file(self.api_key.to_string(), self.web_chat_path.to_string());
+                            println!("Saving API Key - {}", self.api_key.to_string());
+                            functions::save_api_key(&self.api_key).expect("Failed to save API key");
+                            println!("UUID = {}", self.url_search_uuid);
+                        }
+                        if ui.button("Close").clicked() {
+                            self.settings_open = false;
+                        }
+                    });
+                    
+                });
+            }
+
+            ui.separator();
+
+            ui.vertical_centered(|ui| {
+                ui.heading("Portable URLScan GUI");
+            });
+
+            ui.separator();
+            ui.add_space(5.0);
+
+            // Check for API Key
+            if self.api_key == "" {
+                if functions::get_api_key().expect("").is_empty() {
+                    ui.add_space(5.0);
+                    ui.colored_label(egui::Color32::RED, "API KEY MISSING - ADD API KEY TO SETTINGS");
+                    ui.separator();
+                } else {
+                    self.api_key = functions::get_api_key().expect("Weird Failure")
+                }
+                
+            }
+
+            ui.horizontal(|ui| {
+                ui.label("URL to scan:");
+                ui.text_edit_singleline(&mut self.url_to_scan);
+                if ui.button("Scan URL").clicked() {
+
+                    if self.results.ips != Vec::<String>::default() {
+                        self.results = ResultsData {
+                            community_malicious: Default::default(),
+                            community_score: Default::default(),
+                            countries: Default::default(),
+                            ips: Default::default(),
+                            overall_malicious: Default::default(),
+                            overall_score: Default::default(),
+                            urls: Default::default(),
+                        };
+                    }
+
+                    let blank = &self.url_to_scan;
+                    self.current_url_name = blank.to_string();
+                    // self.current_url_name = &self.url_to_scan;
+                    println!("WOOOO");
+                    println!("{} {}", self.url_to_scan.to_string(), self.api_key.to_string());
+                    self.url_search_uuid = String::new();
+
+                    functions::scan_url(self.url_to_scan.to_string(), self.api_key.to_string());//.expect("Failed to scan url"); 
+
+                    self.url_search_uuid = functions::get_uuid().expect("Couldn't retrieve UUID");
+
+
+                    }
+            });
+
+            if !self.url_search_uuid.is_empty() {
+                ui.add_space(10.0);
+                ui.colored_label(egui::Color32::GREEN, "URL search created, please wait roughly 15secs before gathering results.");
+                ui.colored_label(egui::Color32::LIGHT_YELLOW, format!("UUID = {}", self.url_search_uuid));
+                ui.colored_label(egui::Color32::LIGHT_YELLOW, format!("Current URL = {}", self.current_url_name));
+
+                ui.add_space(10.0);
+                ui.vertical_centered(|ui| {
+                    if ui.button("Fetch results").clicked() {
+                        functions::fetch_results().expect("Failed to fetch results");
+
+                        self.results = functions::load_data();
+
+                        println!("{:?}", self.results.ips);
+
+                    }
+                });
+                
+            }
+            ui.add_space(20.0);
+
+            if self.results.ips != Vec::<String>::default() {
+                ui.horizontal(|ui| {
+                    ui.colored_label(egui::Color32::GREEN, "IPs: ");
+                    ui.vertical_centered(|ui| {
+                        // ui.text_edit_multiline(&mut self.results.ips.join("\n "));
+                        egui::ScrollArea::vertical().id_source("ips_scroll_area").show(ui, |ui| {
+                        ui.add(egui::TextEdit::multiline(&mut self.results.ips.join("\n")).desired_rows(5));
+                    });
+                        });
+                    
+                });
+                ui.add_space(5.0);
+                ui.horizontal(|ui| {
+                    ui.colored_label(egui::Color32::GREEN, "URLs: ");
+                   // ui.add(egui::TextEdit::multiline(&mut self.results.urls.join("\n\n")).desired_rows(5);
+                    ui.vertical_centered(|ui| {
+                        egui::ScrollArea::vertical().id_source("urls_scroll_area").show(ui, |ui| {
+                        ui.add(egui::TextEdit::multiline(&mut self.results.urls.join("\n\n")).desired_rows(5));
+                    });
+                    });
+                    
+                });
+                ui.add_space(5.0);
+                ui.horizontal(|ui| {
+                    ui.colored_label(egui::Color32::GREEN, "Countries: ");
+                    ui.vertical_centered(|ui| {
+                        // ui.text_edit_multiline(&mut self.results.countries.join("\n "));
+                        egui::ScrollArea::vertical().id_source("countries_scroll_area").show(ui, |ui| {
+                        ui.add(egui::TextEdit::multiline(&mut self.results.countries.join("\n")).desired_rows(5));
+                    });
+                        });
+                    
+                });
+                ui.add_space(5.0);
+                ui.horizontal(|ui| {
+                    ui.colored_label(egui::Color32::GREEN, "Overall score: ");
+                    ui.colored_label(egui::Color32::RED, self.results.overall_score.to_string());
+                });
+                ui.add_space(5.0);
+                ui.horizontal(|ui| {
+                    ui.colored_label(egui::Color32::GREEN, "Overall malicious? ");
+                    ui.colored_label(egui::Color32::RED, self.results.overall_malicious.to_string());
+                });
+                ui.add_space(5.0);
+                ui.horizontal(|ui| {
+                    ui.colored_label(egui::Color32::GREEN, "Community score: ");
+                    ui.colored_label(egui::Color32::RED, self.results.community_score.to_string());
+                });
+                ui.add_space(5.0);
+                ui.horizontal(|ui| {
+                    ui.colored_label(egui::Color32::GREEN, "Community malicious?: ");
+                    ui.colored_label(egui::Color32::RED, self.results.community_malicious.to_string());
+                });
+            }
+        });
+    }
+}
